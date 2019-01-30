31bed277b09bea0edcbd611fa2b215cb4f8003a0
chengzichao <592292495@qq.com>
2019-01-30 17:40:53 +0800

    finish login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  public loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,15 +1,25 @@
-import { MatInputModule, MatFormFieldModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule } from '@angular/material';
+
 import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
 @NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    LoginRoutingModule,
+  ],
   declarations: [
     LoginComponent,
   ],
-  imports: [
-    MatInputModule,
-    MatFormFieldModule,
-    CommonModule,
-  ]
 })
 export class LoginModule {}
